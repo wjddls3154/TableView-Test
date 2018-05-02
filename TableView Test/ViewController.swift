eab29d8be0c50d470b105a7f458c9968efe73a8c
D7702_10 <d7702_10@d7702-10ui-imac.local>
2018-05-02 14:30:03 +0900

    Table view upgrade

--- TableView Test/ViewController.swift
+++ TableView Test/ViewController.swift
@@ -10,28 +10,39 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
-    var data = ["1","2","3"]
-    var subData = ["ONE", "TWO", "THREE"]
+    var animals = ["강아지","고양이","돼지"]
+    var country = ["영국", "중국", "미국"]
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return data.count
+        return animals.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let identifier = "RE"
         let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
         
-        cell.textLabel?.text = data[indexPath.row]
+        cell.textLabel?.text = animals[indexPath.row]
         cell.detailTextLabel?.text = "section= \(indexPath.section) row=\(indexPath.row)"
         
         let myImg = UIImage(named:"\(indexPath.row+1).jpg")
 
         cell.imageView?.image = myImg
+        cell.textLabel?.text = animals[indexPath.row]
+        cell.detailTextLabel?.text = country[indexPath.row]
         
         return cell
     }
     
 
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section = \(indexPath.section) row = \(indexPath.row)")
+        print(animals[indexPath.row])
+        print(country[indexPath.row])
+        
+    }
+    
+    
+    
     @IBOutlet weak var myTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
